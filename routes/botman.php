9e8530b4107e6781061584fe89f7bc193f53d948
Lester Hurtado <lester@hurtado.ph>
2018-11-19 06:42:37 +0800

    add /stop and dialogflow small in botman router

--- routes/botman.php
+++ routes/botman.php
@@ -1,4 +1,7 @@
 <?php
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Middleware\Dialogflow;
 use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
@@ -7,3 +10,14 @@ $botman->hears('test', function ($bot) {
     $bot->reply('It works!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->hears('/stop|\s', function(BotMan $bot) {
+	$bot->reply('stopped...');
+})->stopsConversation();
+
+$dialogflow = Dialogflow::create('2a7576f8e70d445c89b6db456e0c3555')->listenForAction();
+$botman->middleware->received($dialogflow);
+
+$botman->fallback(function (BotMan $bot){
+    return $bot->reply($bot->getMessage()->getExtras('apiReply'));
+});
