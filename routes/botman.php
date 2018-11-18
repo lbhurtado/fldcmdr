574be327d66048c64671fb4db251a5d29f3fb7f8
Lester Hurtado <lester@hurtado.ph>
2018-11-19 06:36:34 +0800

    add botman telegram and facebook drivers

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$botman->hears('test', function ($bot) {
+    $bot->reply('It works!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
