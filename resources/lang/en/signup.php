4208bc5d9abcf8c373db71cabd153f5f52a3a75f
Lester Hurtado <lester@hurtado.ph>
2018-11-25 08:26:54 +0800

    add signup.php

---
+++ resources/lang/en/signup.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'input' => [
+		'stub' => "Input stub:",	
+	],
+	'woo' => [
+		'stub' => "Stub: :stub",
+	],
+	'introduction' => "You are invited. Enter /stop anywhere to break the loop.",
+	'processing' => "Please stand by. Processing...",
+	'processed' => "Signup is complet.",
+
+	'fail' => "Something is wrong. Please notify the system administrator.",
+];
