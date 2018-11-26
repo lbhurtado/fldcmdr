7dd23b82f4659e03e97dfe7ec572db9a03c3858c
Lester Hurtado <lester@hurtado.ph>
2018-11-26 09:51:03 +0800

    checking for bugs 4

--- app/TapZone.php
+++ app/TapZone.php
@@ -15,12 +15,12 @@ class TapZone extends Model
 
     public static function generate(User $user, $role = 'subscriber')   
     {
-        return tap(TapZone::byUser($user)->firstOrNew(compact('role')), function ($model) use ($user) {
-            $checkin = Checkin::byUser($user)->latest()->first();
-            $model->longitude = $checkin->longitude;
-            $model->latitude = $checkin->latitude;
-            $model->user()->associate($user);
-            $model->save();            
+        return tap(static::byUser($user)->firstOrNew(compact('role')), function ($model) use ($user) {
+            // $checkin = Checkin::byUser($user)->latest()->first();
+            // $model->longitude = $checkin->longitude;
+            // $model->latitude = $checkin->latitude;
+            // $model->user()->associate($user);
+            // $model->save();            
         })->coordinates;
     }
 
@@ -33,6 +33,11 @@ class TapZone extends Model
 
     public function getCoordinatesAttribute(): array
     {
+        return = [
+            'longitude' => 121.030962,
+            'latitide' => 14.644346,
+        ];
+
         return [
             'longitude' => $this->longitude,
             'latitide' => $this->latitide,
