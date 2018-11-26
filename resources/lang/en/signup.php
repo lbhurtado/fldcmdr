00f1c87e6f2113e38c6cc5f9a76f2b653fccea5a
Lester Hurtado <lester@hurtado.ph>
2018-11-26 10:34:31 +0800

    checking for bugs 18

--- resources/lang/en/signup.php
+++ resources/lang/en/signup.php
@@ -10,6 +10,8 @@ return [
 	'introduction' => "You are invited. Enter /stop anywhere to break the loop.",
 	'processing' => "Please stand by. Processing...",
 	'processed' => "Signup is complete.",
-
+	'fence' => [
+		'center' = "longitude = :longitude, latitude = :latitude",
+	],
 	'fail' => "Something is wrong. Please notify the system administrator.",
 ];
