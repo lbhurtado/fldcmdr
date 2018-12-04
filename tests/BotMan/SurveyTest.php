07569ff00919063cab6b5c6343f057ed8a15178a
Lester Hurtado <lester@hurtado.ph>
2018-12-04 18:34:43 +0800

    test actual survey data, but not yet finished

--- tests/BotMan/SurveyTest.php
+++ tests/BotMan/SurveyTest.php
@@ -4,8 +4,8 @@ namespace Tests\BotMan;
 
 use Tests\TestCase;
 
-use App\User;
 use App\Category;
+use App\{User, Invitee};
 use App\Question as SurveyQuestion;
 use App\Eloquent\Phone;
 use BotMan\Drivers\Telegram\TelegramDriver;
@@ -31,6 +31,8 @@ class SurveyTest extends TestCase
         $survey->each(function ($category) {
             $createdCategory = Category::create([
                 'title' => $category['category'],
+                'type' => $category['type'],
+                'enabled_at' => $category['enabled_at'],
                 'extra_attributes' => $category['options'] ?? [],
             ]);
             collect($category['questions'])->each(function ($question) use ($createdCategory) {
@@ -39,10 +41,26 @@ class SurveyTest extends TestCase
                     'question' => $question['question'],
                     'type' => $question['type'],
                     'extra_attributes' => $question['options'] ?? [],
-                    'options' => $question['options'] ?? [],
+                    // 'options' => $question['options'] ?? [],
                 ]);
             });
         });
+
+        // $survey->each(function ($category) {
+        //     $createdCategory = Category::create([
+        //         'title' => $category['category'],
+        //         'extra_attributes' => $category['options'] ?? [],
+        //     ]);
+        //     collect($category['questions'])->each(function ($question) use ($createdCategory) {
+        //         $createdQuestion = SurveyQuestion::create([
+        //             'category_id' => $createdCategory->id,
+        //             'question' => $question['question'],
+        //             'type' => $question['type'],
+        //             'extra_attributes' => $question['options'] ?? [],
+        //             'options' => $question['options'] ?? [],
+        //         ]);
+        //     });
+        // });
 
     }
 
@@ -54,9 +72,11 @@ class SurveyTest extends TestCase
             Phone::number('09178251991'),
             Phone::number('09189362340'),
         ];
-        // $mobile = Phone::number('09178251991');
+
         $category = 'Demographics';
-        $count = 4;
+        $count = 5;
+        $c = Category::where('title', $category)->first();
+
         $coordinate = ['longitude' => 121.030962, 'latitude' => 14.644346];
         $categories = Category::all();
 
@@ -68,51 +88,102 @@ class SurveyTest extends TestCase
                 ->setDriver(TelegramDriver::class)
                 ->receives($this->keyword)
                 ->assertReply(trans('survey.intro'))
+                ->assertQuestion(trans('survey.input.category'))
+                ->receives($c->id)
                 ;
 
-            if ($categories->count() == 0) {
-                $this->bot->assertReply(trans('survey.abort'));
-            }
-            else {
-                if ($categories->count() > 1) {
-                    $this->bot
-                        ->assertQuestion(trans('survey.input.category'))
-                        ->receives(1)
-                        ;
-                }  
-
-                if (config('chatbot.survey.location'))
+            if (config('chatbot.survey.location'))
                 $this->bot
-                    ->assertTemplate(OutgoingMessage::class)
+                    ->assertReply(trans('survey.input.location'))
                     ->receivesLocation($coordinate['latitude'], $coordinate['longitude'])
                     ;
 
-                $this->bot
-                    ->assertReply(trans('survey.info', compact('category', 'count')))
-                    ->assertQuestion(trans('survey.input.mobile'))
-                    ->receives($mobile)
-                    ->assertTemplate(Question::class)
-                    ->receives('Yes')
-                    ->assertReply(trans('survey.answer', ['answer' => 'Yes']))
-                    ->assertTemplate(Question::class)
-                    ->receives('Male')
-                    ->assertReply(trans('survey.answer', ['answer' => 'Male']))
-                    ->assertTemplate(Question::class)
-                    ->receives('18 to 30')
-                    ->assertReply(trans('survey.answer', ['answer' => '18 to 30']))
-                    ->assertTemplate(Question::class)
-                    ->receives('Tondo')
-                    ->assertReply(trans('survey.answer', ['answer' => 'Tondo']))
-                    ->assertReply(trans('survey.finished'))
-                    ;
+            $this->bot
+                ->assertReply(trans('survey.info', compact('category', 'count')))
+                ->assertQuestion(trans('survey.input.mobile'))
+                ->receives($mobile)
+                ;
 
-                \Queue::assertPushed(\App\Jobs\SendAskableReward::class);
-            } 
+            $this->assertDatabaseHas('invitees', compact('mobile'));
+
+            $askable = Invitee::withMobile($mobile)->first();
+            $askable_id = $askable->id;
+            $askable_type = get_class($askable);
+
+            $answer = 'Yes';
+            $this->bot
+                ->assertQuestion(trans('survey.question', [
+                    'current'   => 1,
+                    'count'     => 5,
+                    'text'      => 'Registered Voter?'
+                ]))
+                ->receivesInteractiveMessage($answer)
+                ->assertReply(trans('survey.answer', compact('answer')))
+                ;
+
+            $this->assertDatabaseHas('answers', compact('answer', 'askable_id', 'askable_type')); //add user_id
+
+            $answer = 'Male';
+            $this->bot
+                ->assertQuestion(trans('survey.question', [
+                    'current'   => 2,
+                    'count'     => 5,
+                    'text'      => 'Gender?'
+                ]))
+                ->receivesInteractiveMessage($answer)
+                ->assertReply(trans('survey.answer', compact('answer')))
+                ;
+
+            $this->assertDatabaseHas('answers', compact('answer', 'askable_id', 'askable_type'));
+
+            $answer = 'Class A';
+            $this->bot
+                ->assertQuestion(trans('survey.question', [
+                    'current'   => 3,
+                    'count'     => 5,
+                    'text'      => 'Social Economic Class?'
+                ]))
+                ->receivesInteractiveMessage($answer)
+                ->assertReply(trans('survey.answer', compact('answer')))
+                ;
+
+            $this->assertDatabaseHas('answers', compact('answer', 'askable_id', 'askable_type'));
+
+            $answer = '18 to 30';
+            $this->bot
+                ->assertQuestion(trans('survey.question', [
+                    'current'   => 4,
+                    'count'     => 5,
+                    'text'      => 'Age Group?'
+                ]))
+                ->receivesInteractiveMessage($answer)
+                ->assertReply(trans('survey.answer', compact('answer')))
+                ;
+
+            $this->assertDatabaseHas('answers', compact('answer', 'askable_id', 'askable_type'));
+
+            $answer = 'Tondo';
+            $this->bot
+                ->assertQuestion(trans('survey.question', [
+                    'current'   => 5,
+                    'count'     => 5,
+                    'text'      => 'District in Manila?'
+                ]))
+                ->receivesInteractiveMessage($answer)
+                ->assertReply(trans('survey.answer', compact('answer')))
+                ;
+
+            $this->assertDatabaseHas('answers', compact('answer', 'askable_id', 'askable_type'));
+            
+            $this->bot
+                ->assertReply(trans('survey.finished'))
+                ;                    
+
+            \Queue::assertPushed(\App\Jobs\SendAskableReward::class); 
         }
         
     }
 
-/** @test */
     public function survey_required_question_run()
     {
         $channel_id = $this->faker->randomNumber(8);
@@ -120,8 +191,11 @@ class SurveyTest extends TestCase
             Phone::number('09178251991'),
             Phone::number('09189362340'),
         ];
-        $category = 'Demographics';
-        $count = 4;
+        $c = Category::first();
+        $category = $c->title;
+        $count = $c->count();
+        // $category = 'Demographics';
+        // $count = 4;
         $coordinate = ['longitude' => 121.030962, 'latitude' => 14.644346];
         $categories = Category::all();
 
@@ -205,8 +279,10 @@ class SurveyTest extends TestCase
     private function getData()
     {
         return collect([
-            [
+[
                 'category' => 'Demographics',
+                'type' => 'text',
+                'enabled_at' => now(),
                 'options' => [
                     'twosome' => true,
                     'reward' => 25,
@@ -214,7 +290,7 @@ class SurveyTest extends TestCase
                 ],
                 'questions' => [
                     [
-                        'question' => 'Poll watcher?',
+                        'question' => 'Registered Voter?',
                         'type' => 'radio',
                         'options' => [
                             'required' => true,
@@ -236,6 +312,20 @@ class SurveyTest extends TestCase
                         ],
                     ],
                     [
+                        'question' => 'Social Economic Class?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Class A',
+                                'Class B',
+                                'Class C',
+                                'Class D',
+                                'Class E',
+                            ],
+                        ],
+                    ],
+                    [
                         'question' => 'Age Group?',
                         'type' => 'radio',
                         'options' => [
@@ -249,7 +339,7 @@ class SurveyTest extends TestCase
                         ],
                     ],
                     [
-                        'question' => 'District?',
+                        'question' => 'District in Manila?',
                         'type' => 'radio',
                         'options' => [
                             'required' => false,
@@ -274,6 +364,199 @@ class SurveyTest extends TestCase
                     ],
                 ],
             ],
+[
+                'category' => 'Popular',
+                'type' => 'text',
+                'enabled_at' => now(),
+                'options' => [
+                    'twosome' => false,
+                    'reward' => 0,
+                    'pollcount' => false,
+                ],
+                'questions' => [
+                    [
+                        'question' => 'Who will you vote for in the 2019 elections?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Erap Estrada',
+                                'Isko Moreno',
+                                'Lito Atienza',
+                                'Alfredo Lim',
+                            ],
+                            'anchored' => true,
+                        ],
+                    ],
+                    [
+                        'question' => 'Why :anchor?',
+                        'type' => 'radio',
+                        
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Honest',
+                                'Track Record',
+                                'Popular',
+                                'Rich',
+                            ],
+                        ],
+                    ],
+                    [
+                        'question' => 'Why is <Erap Estrada> not your #1?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Corrupt',
+                                'Gay',
+                                'Tamad',
+                                'Killer',
+                            ],
+                            'anchor_regex' => '<:anchor>',
+                            'anchor_action' => 'skip',
+                        ],
+                    ],
+                    [
+                        'question' => 'Why is <Isko Moreno> not your #1?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Corrupt',
+                                'Gay',
+                                'Tamad',
+                                'Killer',
+                            ],
+                            'anchor_regex' => '<:anchor>',
+                            'anchor_action' => 'skip',
+                        ],
+                    ],
+                    [
+                        'question' => 'Why is <Lito Atienza> not your #1?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Corrupt',
+                                'Gay',
+                                'Tamad',
+                                'Killer',
+                            ],
+                            'anchor_regex' => '<:anchor>',
+                            'anchor_action' => 'skip',
+                        ],
+                    ],
+                    [
+                        'question' => 'Why is <Alfredo Lim> not your #1?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Corrupt',
+                                'Gay',
+                                'Tamad',
+                                'Killer',
+                            ],
+                            'anchor_regex' => '<:anchor>',
+                            'anchor_action' => 'skip',
+                        ],
+                    ],
+                    [
+                        'question' => 'What is the most important issue?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Crime',
+                                'Corruption',
+                                'Environment',
+                            ],
+                        ],
+                    ],
+                    [
+                        'question' => 'What is your problem?',
+                        'type' => 'radio',
+                        'options' => [
+                            'required' => false,
+                            'values' => [
+                                'Health',
+                                'Labor',
+                                'Education',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            // [
+            //     'category' => 'Demographics',
+            //     'options' => [
+            //         'twosome' => true,
+            //         'reward' => 25,
+            //         'pollcount' => false,
+            //     ],
+            //     'questions' => [
+            //         [
+            //             'question' => 'Poll watcher?',
+            //             'type' => 'radio',
+            //             'options' => [
+            //                 'required' => true,
+            //                 'values' => [
+            //                     'Yes',
+            //                     'No'
+            //                 ],
+            //             ],
+            //         ],
+            //         [
+            //             'question' => 'Gender?',
+            //             'type' => 'radio',
+            //             'options' => [
+            //                 'required' => false,
+            //                 'values' => [
+            //                     'Male',
+            //                     'Female'
+            //                 ],
+            //             ],
+            //         ],
+            //         [
+            //             'question' => 'Age Group?',
+            //             'type' => 'radio',
+            //             'options' => [
+            //                 'required' => false,
+            //                 'values' => [
+            //                     '18 to 30',
+            //                     '31 to 40',
+            //                     '41 to 50',
+            //                     '51 and above',
+            //                 ],
+            //             ],
+            //         ],
+            //         [
+            //             'question' => 'District?',
+            //             'type' => 'radio',
+            //             'options' => [
+            //                 'required' => false,
+            //                 'values' => [
+            //                     'Intramuros',
+            //                     'Tondo',
+            //                     'Paco',
+            //                     'Sampaloc',
+            //                     'Sta. Ana',
+            //                     'San Nicolas',
+            //                     'Santa Cruz',
+            //                     'Binondo',
+            //                     'Port Area',
+            //                     'Malate',
+            //                     'Ermita',
+            //                     'San Miguel',
+            //                     'Pandacan',
+            //                     'San Andres',
+            //                     'Santa Mesa',
+            //                 ],
+            //             ],
+            //         ],
+            //     ],
+            // ],
             // [
             //     'category' => 'Popular',
             //     'type' => 'text',
