462b280d63110570691f8dec5100ec1f6c18ed15
Lester Hurtado <lester@hurtado.ph>
2019-01-14 14:01:07 +0800

    create array in SMSSeeder

--- database/seeds/SMSSeeder.php
+++ database/seeds/SMSSeeder.php
@@ -12,31 +12,59 @@ class SMSSeeder extends Seeder
      */
     public function run()
     {
-        SMS::create([
-        	'from' => '+639173011987',
-        	'to' => '+639081877788',
-        	'message' => '@altavista',
-        ]);
-        SMS::create([
-        	'from' => '+639173011987',
-        	'to' => '+639081877788',
-        	'message' => 'regular#levi',
-        ]);
-        SMS::create([
-        	'from' => '+639171111111',
-        	'to' => '+639081877788',
-        	'message' => 'levi Lester',
-        ]);
-        SMS::create([
-        	'from' => '+639171111111',
-        	'to' => '+639081877788',
-        	'message' => '@ambacan',
-        ]);
-        SMS::create([
-            'from' => '+639171111111',
-            'to' => '+639081877788',
-            'message' => '#ambacan',
-        ]);
-        // SMS::create(['from' => '+639172222222', 'to' => '+639081877788', 'message' => 'levi706 Dene']);
+        $smss = $this->getSMSs();
+
+        foreach ($smss as $sms) {
+            SMS::create([
+                'from' => $sms[1],
+                'to' => $sms[0],
+                'message' => $sms[2],
+            ]);            
+        }
+
+        // SMS::create([
+        // 	'from' => '+639173011987',
+        // 	'to' => '+639081877788',
+        // 	'message' => '@altavista',
+        // ]);
+        // SMS::create([
+        // 	'from' => '+639173011987',
+        // 	'to' => '+639081877788',
+        // 	'message' => 'regular#levi',
+        // ]);
+        // SMS::create([
+        // 	'from' => '+639171111111',
+        // 	'to' => '+639081877788',
+        // 	'message' => 'levi Lester',
+        // ]);
+        // SMS::create([
+        // 	'from' => '+639171111111',
+        // 	'to' => '+639081877788',
+        // 	'message' => '@ambacan',
+        // ]);
+        // SMS::create([
+        //     'from' => '+639171111111',
+        //     'to' => '+639081877788',
+        //     'message' => '#ambacan',
+        // ]);
+
+        // SMS::create(['from' => '+639171111111', 'to' => '+639081877788', 'message' => '#ambacan']);
+    }
+
+    protected function getSMSs()
+    {
+        return [
+            ['+639081877788', '+639173011987', '@altavista'                 ],
+            ['+639081877788', '+639173011987', 'regular#levi'               ],
+
+            ['+639081877788', '+639171111111', 'levi Lester'                ],
+            ['+639081877788', '+639171111111', '@ambacan'                   ],
+            // ['+639081877788', '+639171111111', '#ambacan'                   ],
+
+        //     ['+639081877788', '+639172222222', 'ambacan Francesca'          ],
+        //     ['+639081877788', '+639173333333', 'ambacan Sofia'              ],
+        //     ['+639081877788', '+639174444444', 'ambacan Amelia'             ],
+        //     ['+639081877788', '+639175555555', 'ambacan Nicolo'             ],
+        ];
     }
 }
